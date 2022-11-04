b2ea3398e8b4b0662e699523e6546400b0277ed0
cjuliek <cao.juliek@gmail.com>
2022-11-04 10:42:33 -0700

    putting to correct github acc and repo

---
+++ app.js
@@ -0,0 +1,5 @@
+const night_btn = document.getElementById('night_btn');
+
+night_btn.addEventListener('click', function onClick() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = invertColor;} );
