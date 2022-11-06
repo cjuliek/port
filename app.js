d71e131fbe9327fd9895d0d14dfed5dff6e1846c
cjuliek <cao.juliek@gmail.com>
2022-11-05 18:59:05 -0700

    random img generator on smile button working

--- app.js
+++ app.js
@@ -9,27 +9,37 @@ function close() {
   document.getElementById("project_modal").style.display="none";
 }
 
-// -- display img on click of 'smile' -- //
-document.getElementById("smile_trigger").addEventListener("click", reveal);
-function reveal() {
-    document.getElementById("latte").style.display="block"}
-
+// -- Smile = reveal random img of pets -- // 
 pets_array =[
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4354.jpeg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4440.jpeg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4519.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4521 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4522 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4523 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4524 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4525 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4526 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4527 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4528 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4529 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4530 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4531 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_4532 2.jpg',
-   '/Users/jules/GA/Portfolio_JC/images/pets/IMG_6906.jpeg',
+    src="./images/pets/IMG_4354.jpeg",
+    src="./images/pets/IMG_4440.jpeg",
+    src="./images/pets/IMG_4519.jpg",
+    src="./images/pets/IMG_4521 2.jpg",
+    src="./images/pets/IMG_4522 2.jpg",
+    src="./images/pets/IMG_4523 2.jpg",
+    src="./images/pets/IMG_4524 2.jpg",
+    src="./images/pets/IMG_4525 2.jpg",
+    src="./images/pets/IMG_4526 2.jpg",
+    src="./images/pets/IMG_4527 2.jpg",
+    src="./images/pets/IMG_4528 2.jpg",
+    src="./images/pets/IMG_4529 2.jpg",
+    src="./images/pets/IMG_4530 2.jpg",
+    src="./images/pets/IMG_4531 2.jpg",
+    src="/images/pets/IMG_4532 2.jpg",
+    src="./images/pets/IMG_6906.jpeg",
 ]
-function
+function randomImage() {
+    //pull random index of img
+random_index = Math.floor(Math.random() * pets_array.length);
+    // get img at random index
+    selected_image = pets_array[random_index]
+    console.log(selected_image)
+    //update img src to selected img
+    document.getElementById('pets_reveal').src = `${selected_image}`
+    //display img 
+    document.getElementById("pets_reveal").style.display = "block"
+}
+document.getElementById("smile_trigger").addEventListener("click", randomImage);
+function revealRan() {
+    document.getElementById("pets_reveal").style.display = "block";
+}
